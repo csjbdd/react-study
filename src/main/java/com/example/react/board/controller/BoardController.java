c0308d67221fcbb602ce7ea7e74326d01a022139
bdd <csjbdd2017@gmail.com>
2023-01-04 10:05:47 +0900

    commit

--- src/main/java/com/example/react/board/controller/BoardController.java
+++ src/main/java/com/example/react/board/controller/BoardController.java
@@ -1,19 +1,29 @@
 package com.example.react.board.controller;
 
+import com.example.react.board.Entity.Board;
 import com.example.react.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class BoardController {
+
+    @Autowired
     BoardRepository boardRepository;
     @GetMapping("/test")
-    public String test() {
-        return "test";
+    public List<Board> test() {
+       List<Board> boardList = boardRepository.findAll();
+
+
+        return boardList;
     }
 
 }
--- src/main/java/com/example/react/board/repository/BoardRepository.java
+++ src/main/java/com/example/react/board/repository/BoardRepository.java
@@ -1,5 +1,6 @@
 package com.example.react.board.repository;
 
+import com.example.react.board.Entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
